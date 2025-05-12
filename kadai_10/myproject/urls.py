877bdbcc1747f8eb07992b0dda7a8344367b2cda
keidon1988 <k-ota@assuloan.com>
2025-05-12 14:07:00 +0900

    Add files via upload

--- kadai_10/myproject/urls.py
+++ kadai_10/myproject/urls.py
@@ -21,6 +21,6 @@ from crud import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.TopView.as_view(), name="top"),
-    path('crud/', views.ProductListView.as_view(), name="list"),
-    path('crud/', views.ProductDetailView.as_view(), name="Detail"),
+    path('crud/1', views.ProductListView.as_view(), name="list"),
+    path('crud/detail/2', views.ProductDetailView.as_view(), name="Detail"),
 ]
